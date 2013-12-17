003945128cd413bde6507d83d9553e6dc2c2d776
Yusuke Kanda <uskanda@gmail.com>
2013-12-17 12:42:25 +0900

    add main view

---
+++ oekaki/oekaki/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  oekaki
+//
+//  Created by Yusuke Kanda on 12/17/13.
+//  Copyright (c) 2013 Yusuke Kanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
