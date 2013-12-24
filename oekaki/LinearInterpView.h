c2688a281c2e0e8b0dabcd7b02b76c0611a263c6
Toma Takeda <toma@tomac.local>
2013-12-24 20:27:39 +0900

    黒い線で落書きできます．

---
+++ oekaki/LinearInterpView.h
@@ -0,0 +1,13 @@
+//
+//  LinearInterpView.h
+//  oekaki
+//
+//  Created by Toma Takeda on 2013/12/24.
+//  Copyright (c) 2013年 Yusuke Kanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinearInterpView : UIView
+
+@end
--- oekaki/oekaki/MainViewController.m
+++ oekaki/oekaki/MainViewController.m
@@ -36,4 +36,5 @@
     // Dispose of any resources that can be recreated.
 }
 
+
 @end
